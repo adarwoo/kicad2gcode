ee85458bdccfe6be5cc6485b2d64163218232338
adarwoo <44254473+adarwoo@users.noreply.github.com>
2023-08-16 19:36:23 +0000

    Still working on it

---
+++ pcb2gcode/stock_room.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Defines the StockRoom class.
+# This class defines a virtual stock room storing Consumables
+# (stock with normal wear and tear) and fixtures.
+
+# The stockroom is persisted in a Yaml file.
+# The idea, is that the Yaml could be edited - but also persisted.
+
+class StockItem:
+    """
+    The base class for all items stored in the stock room
+    """
+    def __init__(self):
+        pass
+
+
+class Consumable(StockItem):
+    def __init__(self):
+        pass
+
+
+class CuttingTool(Consumable):
+    def __init__(self, diameter, units=):
+        self.diameter_um
+
+class Fixture
+
+
+class StockRoom:
+    pass
+
+class 
